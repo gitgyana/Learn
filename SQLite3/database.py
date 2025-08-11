d61be2a8e78c78af996de48fd4bcbf7e19c0e616
Gyana Priyadarshi <gyana.career@gmail.com>
2025-08-12 01:20:52 +0530

    Format results

--- SQLite3/database.py
+++ SQLite3/database.py
@@ -8,13 +8,18 @@ c = conn.cursor()
 
 # Quert The Database
 c.execute("SELECT * FROM customers")
-print(c.fetchone())
-print(c.fetchmany(3))
-print(c.fetchall())
+# print(c.fetchone())
+# print(c.fetchmany(3))
+
+items = c.fetchall()
+
+print("NAME " + "\t\tEMAIL")
+print("-----" + "\t\t" + "--------")
+for item in items:
+    print(item[0] + " " + item[1]  + "\t\t" + item[2])
 
 
 # print("Command executed successfully...")
-
 # Commit our command
 conn.commit()
 
