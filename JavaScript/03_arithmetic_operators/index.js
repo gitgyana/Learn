f78c31c9f5a5cda5477e90d8871128484cb4fcd3
Gyana Priyadarshi <gyana.career@gmail.com>
2025-08-28 12:00:04 +0530

    Arithmetic Operators

---
+++ JavaScript/03_arithmetic_operators/index.js
@@ -0,0 +1,44 @@
+// arithmetic operators = operands (values, variables, etc.)
+//                        operators (+ - * /)
+//                        e.g., l1 = x + 5;
+
+let students = 31;
+
+/*
+students = students + 1;
+students = students - 1;
+students = students * 2;
+students = students / 2;
+students = students ** 3
+let extraStudents = students % 2;
+console.log(extraStudents);
+*/
+
+/*
+// Augmented Assignment Operators
+students += 1;
+students -= 1;
+students *= 2;
+students /= 2;
+students **= 2;
+students %= 2;
+*/
+
+/*
+students++;
+students--;
+*/
+
+// console.log(students);
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication & division * modulo
+    4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 16;
+
+console.log(result);
