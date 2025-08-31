7c5e5bfae206d38223a11e5e20bcff9a5aead2b6
Gyana Priyadarshi <gyana.career@gmail.com>
2025-08-31 15:30:59 +0530

    Strict equality

---
+++ JavaScript/18_strict_equality/index.js
@@ -0,0 +1,36 @@
+//   = assignment operator
+//  == comparison operator (compare if values are equal)
+// === strict equality operator (compare of values & datatype are equal)
+//  != inequality operator
+// !== strict inequality operator
+
+const PI = 3.14
+
+if(PI == "3.14") {
+    console.log("That is Pi");
+}
+else {
+    console.log("This is NOT Pi");
+}
+
+if(PI === "3.14") {
+    console.log("That is Pi");
+}
+else {
+    console.log("This is NOT Pi");
+}
+
+if(PI != "3.14") {
+    console.log("That is NOT Pi");
+}
+else {
+    console.log("This is Pi");
+}
+
+if(PI !== "3.14") {
+    console.log("That is NOT Pi");
+}
+else {
+    console.log("This is Pi");
+}
+
